5aeaa1edba09b554de7807214793e28e99804165
Kunal Wagle <kmw13@ic.ac.uk>
2017-02-02 17:03:18 +0000

    Added API Connections for Metro

--- src/com/newsaggregator/Main.java
+++ src/com/newsaggregator/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Cricinfo ap = new Cricinfo();
+            Metro ap = new Metro();
             List<OutletArticle> articles = new ArrayList<>(ap.getArticles());
             for (OutletArticle article : articles) {
                 System.out.println(article.getBody());
