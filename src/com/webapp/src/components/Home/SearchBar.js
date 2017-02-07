4944f83276420ab2ecc329c1773272471b881827
Kunal Wagle <kmw13@ic.ac.uk>
2017-02-07 21:59:52 +0000

    Added dummy navigation bar

--- src/com/webapp/src/components/Home/SearchBar.js
+++ src/com/webapp/src/components/Home/SearchBar.js
@@ -5,7 +5,15 @@ import React from "react";
 import {Jumbotron, Button, Form, FormGroup, FormControl} from "react-bootstrap";
 
 export const SearchBar = ({searchValue, handleSearchValueChanged, handleSearchButtonPressed}) => (
-    <Jumbotron>
+    <Jumbotron className="col-md-8 col-md-offset-2">
+        <h2 style={{"textAlign": "center"}}>
+            News Aggregator and Summariser
+        </h2>
+        <br></br>
+        <p style={{"textAlign": "center"}}>
+            I really need to think of a better name...
+        </p>
+        <br></br>
         <p className="col-md-6 col-md-offset-3">
             <Form inline type="submit">
                 <FormGroup bsClass="col-lg-10" controlId="searchForm">
--- src/com/webapp/src/index.js
+++ src/com/webapp/src/index.js
@@ -7,20 +7,29 @@ import {Router, Route, browserHistory} from "react-router";
 import App from "./App";
 import "babel-polyfill";
 import thunk from "redux-thunk";
+import {NavBarComponent} from "./components/NavBar";
 //import {reducers} from './reducers'
 
 const store = createStore(App, compose(applyMiddleware(thunk)));
 const rootEl = document.getElementById('root');
+const navBar = document.getElementById('navbar');
 
-const render = () => ReactDOM.render((
-        <Provider store={store}>
-            <Router history={browserHistory}>
-                <Route path="/" component={SearchBarContainer}/>
-            </Router>
-        </Provider>
-    ),
-    rootEl
-);
+const render = () => {
+    ReactDOM.render((
+            <NavBarComponent/>
+        ),
+        navBar
+    );
+    ReactDOM.render((
+            <Provider store={store}>
+                <Router history={browserHistory}>
+                    <Route path="/" component={SearchBarContainer}/>
+                </Router>
+            </Provider>
+        ),
+        rootEl
+    );
+}
 
 render();
 
