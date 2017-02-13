85fb7342787244afb2acfff52508bf82d901e7bb
Kunal Wagle <kmw13@ic.ac.uk>
2017-02-13 19:31:42 +0000

    Added dummy test

---
+++ src/test/java/test/java/TopicLabellingTest.java
@@ -0,0 +1,16 @@
+package test.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by kunalwagle on 13/02/2017.
+ */
+public class TopicLabellingTest {
+
+    @Test
+    public void generateTopicLabel() throws Exception {
+        Assert.fail();
+    }
+
+}
