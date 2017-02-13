74ccef445bb183b0b0ebd80f3566c03a60117d0a
Kunal Wagle <kmw13@ic.ac.uk>
2017-02-13 21:06:07 +0000

    Added test for Topic Labelling

--- src/test/java/test/java/TopicLabellingTest.java
+++ src/test/java/test/java/TopicLabellingTest.java
@@ -1,16 +1,20 @@
 package test.java;
 
-import org.junit.Assert;
+import com.newsaggregator.base.Topic;
+import com.newsaggregator.ml.labelling.TopicLabelling;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 /**
  * Created by kunalwagle on 13/02/2017.
  */
 public class TopicLabellingTest {
 
-    @Test
+    private Topic topic = Mockito.mock(Topic.class);
+
+    @Test(expected = IndexOutOfBoundsException.class)
     public void generateTopicLabel() throws Exception {
-        Assert.fail();
+        TopicLabelling.generateTopicLabel(topic);
     }
 
 }
