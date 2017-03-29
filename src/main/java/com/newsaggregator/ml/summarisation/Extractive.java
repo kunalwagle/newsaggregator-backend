e4cdbc07870213cd1b9312a5f6fff977af260136
Kunal Wagle <kmw13@ic.ac.uk>
2017-03-29 09:28:29 +0100

    Summariser skeleton written

---
+++ src/main/java/com/newsaggregator/ml/summarisation/Extractive.java
@@ -0,0 +1,9 @@
+package com.newsaggregator.ml.summarisation;
+
+/**
+ * Created by kunalwagle on 29/03/2017.
+ */
+public class Extractive {
+
+
+}
---
+++ src/main/java/com/newsaggregator/ml/summarisation/Summariser.java
@@ -0,0 +1,14 @@
+package com.newsaggregator.ml.summarisation;
+
+import com.newsaggregator.ml.clustering.Cluster;
+
+/**
+ * Created by kunalwagle on 29/03/2017.
+ */
+public class Summariser {
+
+    public static String summarise(Cluster cluster) {
+        return null;
+    }
+
+}
