d33a630acf205f7520fce6de2fcc97d2c4e0426a
Kunal Wagle <kmw13@ic.ac.uk>
2017-04-13 15:02:26 +0100

    Created basic Wordnet

---
+++ src/main/java/com/newsaggregator/ml/nlp/wordnet/Wordnet.java
@@ -0,0 +1,31 @@
+package com.newsaggregator.ml.nlp.wordnet;
+
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.IDictionary;
+import edu.mit.jwi.item.IIndexWord;
+import edu.mit.jwi.item.POS;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by kunalwagle on 13/04/2017.
+ */
+public class Wordnet {
+
+    private IDictionary dict;
+
+    public Wordnet() throws WordnetFailedToOpenException, IOException {
+        URL url = getClass().getResource("/dict");
+        if (url == null) {
+            throw new WordnetFailedToOpenException();
+        }
+        dict = new Dictionary(url);
+        dict.open();
+    }
+
+    public IIndexWord getWord(String word, POS syntax) {
+        return dict.getIndexWord(word, syntax);
+    }
+
+}
---
+++ src/main/java/com/newsaggregator/ml/nlp/wordnet/WordnetFailedToOpenException.java
@@ -0,0 +1,7 @@
+package com.newsaggregator.ml.nlp.wordnet;
+
+/**
+ * Created by kunalwagle on 13/04/2017.
+ */
+public class WordnetFailedToOpenException extends Exception {
+}
