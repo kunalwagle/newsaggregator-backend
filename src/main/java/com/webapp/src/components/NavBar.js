cae565401a86d6b4dfd2fbda4ebf1b9b82d43c74
Kunal Wagle <kmw13@ic.ac.uk>
2017-04-21 00:28:04 +0100

    New Nav Bar modals

--- src/main/java/com/webapp/src/components/NavBar.js
+++ src/main/java/com/webapp/src/components/NavBar.js
@@ -2,49 +2,77 @@
  * Created by kunalwagle on 07/02/2017.
  */
 import React from "react";
-import {Navbar, Nav, NavItem, OverlayTrigger, Popover} from "react-bootstrap";
-import FacebookLogin from "react-facebook-login";
-import GoogleLogin from "react-google-login";
+import {Navbar, Nav, NavItem, OverlayTrigger, Popover, Button} from "react-bootstrap";
 
-const loginPopover = (handleFacebookLogin, handleGoogleSuccess, handleGoogleFailure) => (
-    <Popover title="Login" id="loginModalPopover">
-        <FacebookLogin appId="540333079424464"
-                       autoLoad={false}
-                       fields="name,email,picture"
-                       callback={handleFacebookLogin}/>
-        <br/><br/><br/>
-        <GoogleLogin onSuccess={handleGoogleSuccess}
-                     onFailure={handleGoogleFailure}
-                     clientId="454694778698-pcb41ncbaqevjkcgvnko36faneenrb73.apps.googleusercontent.com"/>
-    </Popover>
-);
 
-export const NavBarComponent = ({handleFacebookLogin, handleGoogleSuccess, handleGoogleFailure}) => (
-    <div>
-    <Navbar inverse collapseOnSelect>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <a href="#">News Aggregator</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-            <Nav>
-                <NavItem eventKey={1} href="#">About</NavItem>
-            </Nav>
-            <Nav pullRight>
-                <NavItem eventKey={1}>
-                    <input placeholder="Search"/>
-                </NavItem>
-                <OverlayTrigger trigger="click" placement="bottom"
-                                overlay={loginPopover(handleFacebookLogin, handleGoogleSuccess, handleGoogleFailure)}>
+const loginPopover = (loggedIn, emailAddress, handleEmailChange, handleLoginClicked) => {
+
+    if (loggedIn) {
+        return (
+            <Popover title="Log Out" id="loginModalPopover">
+                You are logged in as <strong>{emailAddress}</strong>
+                <Button onClick={handleLoginClicked}>Log Out</Button>
+            </Popover>
+        )
+    } else {
+        return (
+            <Popover title="Login" id="loginModalPopover">
+                <strong>Give an email address to log in</strong>
+                <input onChange={handleEmailChange} placeholder="Email address"/>
+                <Button onClick={handleLoginClicked}>Login</Button>
+            </Popover>
+        );
+    }
+};
+
+const navItems = (loggedIn, emailAddress, handleEmailChange, handleLoginClicked) => {
+    if (loggedIn) {
+        return (
+            <div>
+                <OverlayTrigger rootClose
+                                overlay={loginPopover(loggedIn, emailAddress, handleEmailChange, handleLoginClicked)}>
                     <NavItem eventKey={2}>Register</NavItem>
                 </OverlayTrigger>
+                <OverlayTrigger rootClose
+                                overlay={loginPopover(loggedIn, emailAddress, handleEmailChange, handleLoginClicked)}>
+                    <NavItem eventKey={3}>Sign In</NavItem>
+                </OverlayTrigger>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <NavItem href="myTopics" eventKey={2}>My Topics</NavItem>
+                <OverlayTrigger rootClose
+                                overlay={loginPopover(loggedIn, emailAddress, handleEmailChange, handleLoginClicked)}>
+                    <NavItem eventKey={3}>Log Out</NavItem>
+                </OverlayTrigger>
+            </div>
+        )
+    }
+};
 
-                <NavItem eventKey={3}>Sign In</NavItem>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
+export const NavBarComponent = ({loggedIn, emailAddress, handleEmailChange, handleLoginClicked}) => (
+    <div>
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#">News Aggregator</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav>
+                    <NavItem eventKey={1} href="#">About</NavItem>
+                </Nav>
+                <Nav pullRight>
+                    <NavItem eventKey={1}>
+                        <input placeholder="Search"/>
+                    </NavItem>
+                    {navItems(loggedIn, emailAddress, handleEmailChange, handleLoginClicked)}
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     </div>
 
 );
