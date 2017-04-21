e7e262fa1330338db21e2f5d3ffd9efff510eec8
Kunal Wagle <kmw13@ic.ac.uk>
2017-04-21 02:54:01 +0100

    Reducers added

--- src/main/java/com/webapp/src/reducers/LoginModalReducers.js
+++ src/main/java/com/webapp/src/reducers/LoginModalReducers.js
@@ -1,11 +1,13 @@
 /**
  * Created by kunalwagle on 20/04/2017.
  */
-import {EMAIL_CHANGED, LOG_IN_CHANGE} from "../actions/LoginModalActions";
+import {EMAIL_CHANGED, LOG_IN_CHANGE, FETCH_ENDED, FETCH_STARTED} from "../actions/LoginModalActions";
 
 const initialState = {
     loggedIn: false,
-    email: ""
+    email: "",
+    fetchInProgress: false,
+    topics: []
 };
 
 export default function loggedIn(state, action) {
@@ -26,6 +28,15 @@ export default function loggedIn(state, action) {
             return Object.assign({}, state, {
                 loggedIn: action.loggedIn,
                 email: email
+            });
+        case FETCH_STARTED:
+            return Object.assign({}, state, {
+                fetchInProgress: true
+            });
+        case FETCH_ENDED:
+            return Object.assign({}, state, {
+                topics: action.json,
+                fetchInProgress: false
             });
         default:
             return state
