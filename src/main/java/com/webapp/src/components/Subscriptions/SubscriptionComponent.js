221d107dbfc4aad1bc1f3bd40958017d52d34dea
Kunal Wagle <kmw13@ic.ac.uk>
2017-04-21 03:13:30 +0100

    Created subscription component

---
+++ src/main/java/com/webapp/src/components/Subscriptions/SubscriptionComponent.js
@@ -0,0 +1,42 @@
+/**
+ * Created by kunalwagle on 21/04/2017.
+ */
+import React from "react";
+import TopicPanelContainer from "../../containers/TopicViewer/TopicPanelContainer";
+
+export const SubscriptionComponent = ({topics, handleTopicChange}) => {
+
+    const tabMap = topics.map((topic, index) => {
+        return (
+            <NavItem onSelect={() => handleTopicChange(topic.clusterHolders)} eventKey={index} key={index}>
+                {topic.topic}
+            </NavItem>
+        )
+    });
+
+    const contentMap = topics.map((topic, index) => {
+        return (
+            <Tab.Pane eventKey={index} key={index}>
+                <TopicPanelContainer/>
+            </Tab.Pane>
+        )
+    });
+
+    return (
+        <Tab.Container defaultActiveKey={0}>
+            <Row className="clearfix">
+                <Col sm={4}>
+                    <Nav bsStyle="pills" stacked>
+                        {tabMap}
+                    </Nav>
+                </Col>
+                <Col sm={8}>
+                    <Tab.Content animation>
+                        {contentMap}
+                    </Tab.Content>
+                </Col>
+            </Row>
+        </Tab.Container>
+    )
+
+};
