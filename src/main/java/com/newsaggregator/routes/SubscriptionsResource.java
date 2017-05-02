df2a94a1e432ff4633bd050965f485e2a4d7d450
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-02 13:20:58 +0100

    Return just topic ids in generic user call

--- src/main/java/com/newsaggregator/routes/SubscriptionsResource.java
+++ src/main/java/com/newsaggregator/routes/SubscriptionsResource.java
@@ -4,18 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoDatabase;
 import com.newsaggregator.Utils;
 import com.newsaggregator.base.User;
-import com.newsaggregator.db.Topics;
 import com.newsaggregator.db.Users;
-import com.newsaggregator.server.ClusterHolder;
-import com.newsaggregator.server.LabelHolder;
-import com.newsaggregator.server.TopicHolder;
 import org.apache.log4j.Logger;
 import org.restlet.data.Status;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by kunalwagle on 21/04/2017.
@@ -31,18 +24,19 @@ public class SubscriptionsResource extends ServerResource {
         Users userManager = new Users(db);
         try {
             User response = userManager.getSingleUser(user);
-            List<String> topics = response.getTopicIds();
-            Topics topicManager = new Topics(db);
-            List<TopicHolder> topicHolders = new ArrayList<>();
-            for (String topic : topics) {
-                LabelHolder labelHolder = topicManager.getTopic(topic);
-                List<ClusterHolder> clusterHolders = new ArrayList<>();
-                if (labelHolder != null) {
-                    clusterHolders = labelHolder.getClusters();
-                }
-                topicHolders.add(new TopicHolder(topic, clusterHolders));
-            }
-            return new ObjectMapper().writeValueAsString(topicHolders);
+//            List<String> topics = response.getTopicIds();
+//            Topics topicManager = new Topics(db);
+//            List<TopicHolder> topicHolders = new ArrayList<>();
+//            for (String topic : topics) {
+//                LabelHolder labelHolder = topicManager.getTopicById(topic);
+//                List<ClusterHolder> clusterHolders = new ArrayList<>();
+//                if (labelHolder != null) {
+//                    clusterHolders = labelHolder.getClusters();
+//                }
+//                topicHolders.add(new TopicHolder(topic, clusterHolders));
+//            }
+//            return new ObjectMapper().writeValueAsString(topicHolders);
+            return new ObjectMapper().writeValueAsString(response);
         } catch (Exception e) {
             logger.error("Exception occurred subscribing user", e);
             getResponse().setStatus(Status.SERVER_ERROR_INTERNAL);
