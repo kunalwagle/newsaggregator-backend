13fc530e799179011a13cf179f93f3495160b966
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-04 12:15:13 +0100

    Ready for merging into server

--- src/main/java/com/newsaggregator/Main.java
+++ src/main/java/com/newsaggregator/Main.java
@@ -1,9 +1,13 @@
 package com.newsaggregator;
 
 import com.newsaggregator.routes.RouterApplication;
+import com.newsaggregator.server.jobs.ArticleFetchRunnable;
+import com.newsaggregator.server.jobs.SendEmailRunnable;
 import org.restlet.Component;
 import org.restlet.data.Protocol;
 import org.restlet.service.TaskService;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
@@ -37,8 +41,8 @@ public class Main {
                 TaskService scheduleManager = new TaskService();
 
 //                scheduleManager.schedule(new RemoveDuplicatesRunnable(), 1L, TimeUnit.SECONDS);
-//                scheduleManager.scheduleWithFixedDelay(new ArticleFetchRunnable(), 1L, 10L, TimeUnit.MINUTES);
-//                scheduleManager.scheduleAtFixedRate(new SendEmailRunnable(), 1L, 1L, TimeUnit.HOURS);
+                scheduleManager.scheduleWithFixedDelay(new ArticleFetchRunnable(), 1L, 1L, TimeUnit.MINUTES);
+                scheduleManager.scheduleAtFixedRate(new SendEmailRunnable(), 1L, 1L, TimeUnit.HOURS);
 
                 serverInitialised = true;
 
