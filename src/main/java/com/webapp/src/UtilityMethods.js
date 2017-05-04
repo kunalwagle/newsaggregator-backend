301fff0fa2146e5b1d5f531099683afadbb8689f
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-04 10:27:17 +0100

    Created utility methods

---
+++ src/main/java/com/webapp/src/UtilityMethods.js
@@ -0,0 +1,38 @@
+/**
+ * Created by kunalwagle on 04/05/2017.
+ */
+
+export const getIPAddress = () => {
+    return "http://localhost:8182/api/";
+};
+
+export const getColour = (source) => {
+    switch (source) {
+        case "the-guardian-uk":
+            return "teal";
+        case "independent":
+            return "yellow";
+        case "associated-press":
+            return "lightgreen";
+        case "reuters":
+            return "brown";
+        case "business-insider-uk":
+            return "green";
+        case "daily-mail":
+            return "purple";
+        case "espn-cric-info":
+            return "lightblue";
+        case "metro":
+            return "gold";
+        case "mirror":
+            return "red";
+        case "newsweek":
+            return "magenta";
+        case "the-telegraph":
+            return "darkblue";
+        case "the-times-of-india":
+            return "orange";
+        default:
+            return "black";
+    }
+};
