0ecec371c622a29b18451b73f8c570f98bc88be9
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-05 13:45:13 +0100

    Basic jumbotron styling

--- src/main/java/com/webapp/src/components/Home/SearchBarJumbotron.js
+++ src/main/java/com/webapp/src/components/Home/SearchBarJumbotron.js
@@ -7,17 +7,17 @@ import {Link} from "react-router";
 import SearchBarContainer from "../../containers/Home/SearchBarContainer";
 
 export const SearchBarJumbotron = () => (
-    <Jumbotron className="col-md-8 col-md-offset-2">
-        <h2 style={{"textAlign": "center"}}>
-            News Aggregator and Summariser
-        </h2>
-        <br></br>
-        <p style={{"textAlign": "center"}}>
-            I really need to think of a better name...
-        </p>
-        <br></br>
-        <p className="col-md-6 col-md-offset-3">
+    <div>
+        <Jumbotron className="jumbotron">
+            <h2 style={{"textAlign": "center"}}>
+                News Aggregator and Summariser
+            </h2>
+            <br></br>
+            <p style={{"textAlign": "center"}}>
+                I really need to think of a better name...
+            </p>
+            <br></br>
             <SearchBarContainer/>
-        </p>
-    </Jumbotron>
+        </Jumbotron>
+    </div>
 );
