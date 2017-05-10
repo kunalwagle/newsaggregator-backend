2b664e953a213268c0a76a1bf6cbe8cf789fb81c
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-10 14:47:43 +0100

    Article Viewer complete

--- src/main/java/com/webapp/src/components/ArticleViewer/ArticlesSummarised.js
+++ src/main/java/com/webapp/src/components/ArticleViewer/ArticlesSummarised.js
@@ -12,14 +12,20 @@ export const ArticlesSummarised = ({article, annotations, fetchInProgressCalled,
     }
 
     const articleInfo = article.articles.map((art, index) => {
-        const imageSource = "../../outlets/" + art.source + ".png";
+        const imageSource = "/outlets/" + art.source + ".png";
         if (!annotations) {
             return (
                 <div key={index}>
-                    <img src={imageSource} className="outlet-icon"/>
-                    <b>{art.title}</b>
-                    <br/>
-                    <a href={art.articleUrl}>Original Article</a>
+                    <div>
+                        <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                            <img src={imageSource} className="outlet-icon"/>
+                        </div>
+                        <div className="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                            <b>{art.title}</b>
+                            <br/>
+                            <a href={art.articleUrl}>Original Article</a>
+                        </div>
+                    </div>
                     <br/><br/><br/>
                 </div>
             )
