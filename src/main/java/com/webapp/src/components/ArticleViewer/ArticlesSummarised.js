39b2074b1901401f8a20b143616d09874b4f03f1
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-13 11:40:36 +0100

    Summary Analysis WebApp complete

--- src/main/java/com/webapp/src/components/ArticleViewer/ArticlesSummarised.js
+++ src/main/java/com/webapp/src/components/ArticleViewer/ArticlesSummarised.js
@@ -17,21 +17,21 @@ export const ArticlesSummarised = ({article, sources, annotations, fetchInProgre
         return (<div></div>);
     }
 
-    const articleImage = (imageSource, source) => (
-        <div className="row">
-            <div className="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                <img src={imageSource} alt={imageSource.substring(0, imageSource.length - 3) + "jpg"}
-                     className="outlet-icon"/>
-            </div>
-            <div className="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                <hr style={{"background": getColour(source), "height": "20px"}} className="no-border"/>
-            </div>
-        </div>
-    );
+    // const articleImage = (imageSource, source) => (
+    //     <div className="row">
+    //         <div className="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+    //             <img src={imageSource} alt={imageSource.substring(0, imageSource.length - 3) + "jpg"}
+    //                  className="outlet-icon"/>
+    //         </div>
+    //         <div className="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+    //             <hr style={{"background": getColour(source), "height": "20px"}} className="no-border"/>
+    //         </div>
+    //     </div>
+    // );
 
     const articleInfo = article.articles.map((art, index) => {
         const imageSource = "/outlets/" + art.source + ".png";
-        if (!annotations) {
+        // if (!annotations) {
             return (
                 <div key={index} className="row">
                     <div >
@@ -41,45 +41,45 @@ export const ArticlesSummarised = ({article, sources, annotations, fetchInProgre
                         <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 margin-below">
                             <b>{art.title}</b>
                             <br/>
-                            <a href={art.articleUrl}>Original Article</a>
+                            <a href={art.articleUrl} target="_blank" className="link">Original Article</a>
                         </div>
                         <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4 margin-below">
                             <label className="switch">
                                 <input type="checkbox" checked={contains(sources, art.source)}
                                        disabled={contains(sources, art.source) && sources.length === 1}
                                        onChange={event => handleCheckboxChange(event, art.source)}/>
-                                <div className="slider round"></div>
+                                <div className="slider round" style={{"backgroundColor": getColour(art.source)}}></div>
                             </label>
                         </div>
                     </div>
 
                 </div>
-            )
-        } else {
-            return (
-                <div key={index} className="row">
-                    <div>
-                        <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-                            {articleImage(imageSource, art.source)}
-                        </div>
-                        <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 margin-below">
-                            <b>{art.title}</b>
-                            <br/>
-                            <a href={art.articleUrl}>Original Article</a>
-                        </div>
-                        <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4 margin-below">
-                            <label className="switch">
-                                <input type="checkbox" checked={contains(sources, art.source)}
-                                       disabled={contains(sources, art.source) && sources.length === 1}
-                                       onChange={event => handleCheckboxChange(event, art.source)}/>
-                                <div className="slider round"></div>
-                            </label>
-                        </div>
-                    </div>
-                    <br/><br/><br/><br/>
-                </div>
-            )
-        }
+            );
+        // } else {
+        //     return (
+        //         <div key={index} className="row">
+        //             <div>
+        //                 <div className="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+        //                     {articleImage(imageSource, art.source)}
+        //                 </div>
+        //                 <div className="col-lg-6 col-md-6 col-sm-6 col-xs-6 margin-below">
+        //                     <b>{art.title}</b>
+        //                     <br/>
+        //                     <a href={art.articleUrl}>Original Article</a>
+        //                 </div>
+        //                 <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4 margin-below">
+        //                     <label className="switch">
+        //                         <input type="checkbox" checked={contains(sources, art.source)}
+        //                                disabled={contains(sources, art.source) && sources.length === 1}
+        //                                onChange={event => handleCheckboxChange(event, art.source)}/>
+        //                         <div className="slider round" style={{"backgroundColor":getColour(art.source)}}></div>
+        //                     </label>
+        //                 </div>
+        //             </div>
+        //             <br/><br/><br/><br/>
+        //         </div>
+        //     )
+        // }
     });
 
     let className = "white ";
