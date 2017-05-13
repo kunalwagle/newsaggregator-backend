45a5367b5a9bdee11b46a2edfda3c9290c5b9b5b
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-13 10:25:08 +0100

    Sentence Source Component

--- src/main/java/com/webapp/src/components/ArticleViewer/SentenceSourcesComponent.js
+++ src/main/java/com/webapp/src/components/ArticleViewer/SentenceSourcesComponent.js
@@ -2,8 +2,9 @@
  * Created by kunalwagle on 13/05/2017.
  */
 import React, {Component} from "react";
+import {Popover} from "react-bootstrap";
 
-export default class SentenceSourcesComponent extends Component {
+class SentenceComponent extends Component {
 
     generateImageSource() {
         return "/outlets/" + this.props.source + ".png";
@@ -11,16 +12,38 @@ export default class SentenceSourcesComponent extends Component {
 
     render() {
         return (
-            <div>
+            <div className="row">
                 <div className="col-lg-3 col-md-3 col-sm-3 col-xs-3">
                     <img src={this.generateImageSource()} className="outlet-icon"/>
                 </div>
                 <div className="col-lg-7 col-md-7 col-sm-7 col-xs-7 margin-below">
-                    <b>{this.props.source}</b>
-                    <br/>
-                    <p>{this.props.sentence}</p>
+                    <div className="row">
+                        <b>{this.props.source}</b>
+                    </div>
+                    <div className="row">
+                        <p>{this.props.sentence}</p>
+                    </div>
                 </div>
             </div>
+        )
+    }
+
+}
+
+export default class SentenceSourcesComponent extends Component {
+
+
+    render() {
+        return (
+            <Popover id="sentencePopover" title="Sentence Sources">
+                {
+                    this.props.sentences.map((sentence, index) => {
+                        return (
+                            <SentenceComponent key={index} source={sentence.source} sentence={sentence.sentence}/>
+                        )
+                    })
+                }
+            </Popover>
         )
     }
 
