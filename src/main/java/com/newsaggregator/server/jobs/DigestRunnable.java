9b83aed23f8f09d2ad6c6620dcf3c8efbe7f6fe2
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-17 18:33:39 +0100

    Adding runnables

---
+++ src/main/java/com/newsaggregator/server/jobs/DigestRunnable.java
@@ -0,0 +1,13 @@
+package com.newsaggregator.server.jobs;
+
+/**
+ * Created by kunalwagle on 17/05/2017.
+ */
+public class DigestRunnable implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+}
