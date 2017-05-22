8a68e67812461fe3007e3f8911f691046184b493
Kunal Wagle <kmw13@ic.ac.uk>
2017-05-22 14:24:37 +0100

    BBC News

--- src/main/java/com/newsaggregator/Main.java
+++ src/main/java/com/newsaggregator/Main.java
@@ -1,19 +1,12 @@
 package com.newsaggregator;
 
+import com.newsaggregator.api.outlets.BBCNews;
+import com.newsaggregator.base.OutletArticle;
 import com.newsaggregator.routes.RouterApplication;
-import com.newsaggregator.server.TaskServiceSingleton;
-import com.newsaggregator.server.jobs.ArticleRunnable;
-import com.newsaggregator.server.jobs.DigestRunnable;
-import org.apache.log4j.Logger;
 import org.restlet.Component;
 import org.restlet.data.Protocol;
-import org.restlet.service.TaskService;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 
 public class Main {
@@ -46,29 +39,34 @@ public class Main {
 //            throw new Exception();
 //                new TopicModelling();
 
+                BBCNews bbcNews = new BBCNews();
+                List<OutletArticle> articles = bbcNews.getArticles();
 
-                TaskService scheduleManager = TaskServiceSingleton.getInstance();
+                System.out.println(articles);
 
-                Utils.sendServerRestartEmail(port);
 
-                LocalDateTime localNow = LocalDateTime.now();
-                ZoneId currentZone = ZoneId.of("Europe/London");
-                ZonedDateTime zonedNow = ZonedDateTime.of(localNow, currentZone);
-                ZonedDateTime zonedNext5;
-                zonedNext5 = zonedNow.withHour(11).withMinute(15).withSecond(0);
-                if (zonedNow.compareTo(zonedNext5) > 0)
-                    zonedNext5 = zonedNext5.plusDays(1);
-
-                Duration duration = Duration.between(zonedNow, zonedNext5);
-                long initialDelay = duration.getSeconds();
-
-                Logger.getLogger(Main.class).info("The initial delay will be " + initialDelay);
-//                scheduleManager.scheduleAtFixedRate(new ClusteringScheduleRunnable(), 3L, 15L, TimeUnit.MINUTES);
-//                scheduleManager.scheduleAtFixedRate(new SummarisingScheduleRunnable(), 2L, 15L, TimeUnit.MINUTES);
-//                scheduleManager.scheduleAtFixedRate(new SendEmailRunnable(), 1L, 15L, TimeUnit.MINUTES);
-//                scheduleManager.scheduleAtFixedRate(new LabellingRunnable(), 1L, 15L, TimeUnit.MINUTES);
-                scheduleManager.execute(new ArticleRunnable());
-                scheduleManager.scheduleAtFixedRate(new DigestRunnable(), initialDelay, 24 * 60 * 60, TimeUnit.SECONDS);
+//                TaskService scheduleManager = TaskServiceSingleton.getInstance();
+//
+//                Utils.sendServerRestartEmail(port);
+//
+//                LocalDateTime localNow = LocalDateTime.now();
+//                ZoneId currentZone = ZoneId.of("Europe/London");
+//                ZonedDateTime zonedNow = ZonedDateTime.of(localNow, currentZone);
+//                ZonedDateTime zonedNext5;
+//                zonedNext5 = zonedNow.withHour(11).withMinute(15).withSecond(0);
+//                if (zonedNow.compareTo(zonedNext5) > 0)
+//                    zonedNext5 = zonedNext5.plusDays(1);
+//
+//                Duration duration = Duration.between(zonedNow, zonedNext5);
+//                long initialDelay = duration.getSeconds();
+//
+//                Logger.getLogger(Main.class).info("The initial delay will be " + initialDelay);
+////                scheduleManager.scheduleAtFixedRate(new ClusteringScheduleRunnable(), 3L, 15L, TimeUnit.MINUTES);
+////                scheduleManager.scheduleAtFixedRate(new SummarisingScheduleRunnable(), 2L, 15L, TimeUnit.MINUTES);
+////                scheduleManager.scheduleAtFixedRate(new SendEmailRunnable(), 1L, 15L, TimeUnit.MINUTES);
+////                scheduleManager.scheduleAtFixedRate(new LabellingRunnable(), 1L, 15L, TimeUnit.MINUTES);
+//                scheduleManager.execute(new ArticleRunnable());
+//                scheduleManager.scheduleAtFixedRate(new DigestRunnable(), initialDelay, 24 * 60 * 60, TimeUnit.SECONDS);
 
 
 //                scheduleManager.scheduleAtFixedRate(new ClusteringScheduleRunnable(), 3L, 15L, TimeUnit.MINUTES);
--- src/main/java/com/newsaggregator/api/outlets/BBCNews.java
+++ src/main/java/com/newsaggregator/api/outlets/BBCNews.java
@@ -2,6 +2,10 @@ package com.newsaggregator.api.outlets;
 
 import com.newsaggregator.base.Outlet;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Iterator;
 
 /**
  * Created by kunalwagle on 21/05/2017.
@@ -15,7 +19,24 @@ public class BBCNews extends NewsAPI {
 
     @Override
     protected String extractArticleText(Document page) throws NullPointerException {
-        //TODO Implement
-        return null;
+        Elements articleBody = page.getElementsByClass("story-body__inner").get(0).getElementsByTag("p");
+        Iterator<Element> iterator = articleBody.iterator();
+        boolean hasHaveYourSay = false;
+        while (iterator.hasNext()) {
+            Element element = iterator.next();
+            Elements strong = element.getElementsByTag("strong");
+            strong.remove();
+            strong = element.getElementsByClass("icon email");
+            if (strong.size() > 0) {
+                iterator.remove();
+                hasHaveYourSay = true;
+            }
+        }
+
+        if (hasHaveYourSay) {
+            articleBody.remove(articleBody.size() - 1);
+        }
+
+        return articleBody.text();
     }
 }
