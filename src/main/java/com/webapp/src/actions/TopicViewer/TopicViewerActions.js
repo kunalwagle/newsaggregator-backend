bb11fbee72043dff98444a5f318ec3354c0cb223
Kunal Wagle <kmw13@ic.ac.uk>
2017-06-01 17:53:41 +0100

    Article page reloads correctly

--- src/main/java/com/webapp/src/actions/TopicViewer/TopicViewerActions.js
+++ src/main/java/com/webapp/src/actions/TopicViewer/TopicViewerActions.js
@@ -3,12 +3,14 @@
  */
 import {push} from "react-router-redux";
 import {find} from "underscore";
+import {fetchInProgressToggle} from "../ArticleViewer/ArticleActions";
 
 export const ARTICLE_CLICKED = 'ARTICLE_CLICKED';
 
 export function articleClicked(topicId, articleId) {
     return (dispatch) => {
         dispatch(push("/topic/" + topicId + "/article/" + articleId));
+        dispatch(fetchInProgressToggle());
         return {
             type: ARTICLE_CLICKED
         }
