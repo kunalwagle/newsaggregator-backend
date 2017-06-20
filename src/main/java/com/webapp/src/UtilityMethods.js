61c2aeb21766d7445de35de3c2996f150cf08044
Kunal Wagle <kmw13@ic.ac.uk>
2017-06-20 22:25:12 +0100

    Ready to go

--- src/main/java/com/webapp/src/UtilityMethods.js
+++ src/main/java/com/webapp/src/UtilityMethods.js
@@ -3,7 +3,7 @@
  */
 
 export const getIPAddress = () => {
-    return "http://localhost:8183/api/";
+    return "http://kunalnewsaggregator.co.uk:8182/api/";
 };
 
 export const getColour = (source) => {
